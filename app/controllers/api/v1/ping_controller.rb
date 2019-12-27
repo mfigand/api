463ac24b3a31692327e9e1a09db4c9793eac147e
mfigand <m.figand@gmail.com>
2019-12-27 12:05:19 +0100

    Add requests ping spec

--- app/controllers/api/v1/ping_controller.rb
+++ app/controllers/api/v1/ping_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::PingController < ApplicationController
   def ping
-    render json: { data: 'ok' }
+    # render json: { data: 'ok' }
+    render json: { data: 'pong' }, status: 200
   end
 end
